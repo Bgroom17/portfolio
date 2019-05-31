2198aac5831ce418bb1049d43843b85964e17f40
Bgroom17.github.io <gro15010@byui.edu>
2019-05-31 17:26:53 -0600

    input
    input

--- GroceryGopher/groceryGopher.js
+++ GroceryGopher/groceryGopher.js
@@ -19,13 +19,13 @@ function getGroceryList() {
         var length = groceryList.length;
         for (var i = 0; i < length; i++) {
             createStoredList(groceryList[i]);
-            
+
         }
-        
+
         totalPrice();
     };
-    
-    
+
+
 
 }
 
@@ -38,9 +38,9 @@ function totalPrice() {
     const length = groceryList.length;
     let total = 0;
     for (var i = 0; i < length; i++) {
-        total += parseFloat(groceryList[i].price);
-        var storedPrice = "<li>" + total + "</li>";
-        document.getElementById('totalListPrice').innerHTML = total;
+        total += parseFloat(groceryList[i].price)* parseFloat(groceryList[i].quantity);
+        var storedPrice = "<p>"  + "Total: $"  + total + "</p>";
+        document.getElementById('totalListPrice').innerHTML = storedPrice;
     }
 }
 
@@ -67,69 +67,68 @@ function createStoredList(groceryItem) {
 function createList() {
 
     console.log()
-    
+
     var itemDes = document.getElementById('itemD');
     var itemQ = document.getElementById('quantity');
     var itemP = document.getElementById('price');
-    
-    if(itemDes.value != ''){//all three var
+
+    if (itemDes.value != '' && itemQ.value != '' && itemP.value != '') { //all three var
+
+
+
+        //add item
+
+        var desLi = "<li>" + itemDes.value + "</li>";
+        document.getElementById('itemDescriptionList').innerHTML += desLi;
+
+        //add quantity
+
+        var quantityLi = "<li>" + itemQ.value + "</li>";
+        document.getElementById('itemQuantityList').innerHTML += quantityLi;
+
+        //add price
+
+        var priceLi = "<li>" + itemP.value + "</li>";
+        document.getElementById('itemPriceList').innerHTML += priceLi;
+
+
+
+
+        //adding an item to the array
+        let newItem = {
+            description: itemDes.value,
+            quantity: itemQ.value,
+            price: itemP.value
+        }
+
+        console.log(newItem);
+
+        groceryList.push(newItem);
+
+
+
+
+        //adding the array to local storage
+        window.localStorage.setItem('GroceryList', JSON.stringify(groceryList));
+
+
+        itemDes.value = '';
+        itemQ.value = '';
+        itemP.value = '';
+
+
+    } else {
+        
+        //tell themthat its not working - error messageanddo nothing
+        
        
-       
-
-    //add item
-    
-    var desLi = "<li>" + itemDes.value + "</li>";
-    document.getElementById('itemDescriptionList').innerHTML += desLi;
-
-    //add quantity
-    
-    var quantityLi = "<li>" + itemQ.value + "</li>";
-    document.getElementById('itemQuantityList').innerHTML += quantityLi;
-
-    //add price
-    
-    var priceLi = "<li>" + itemP.value + "</li>";
-    document.getElementById('itemPriceList').innerHTML += priceLi;
-
-
-
-
-    //adding an item to the array
-    let newItem = {
-        description: itemDes,
-        quantity: itemQ,
-        price: itemP
+        document.getElementById('errorMessage').innerHTML= "<p>" +'Error: enter item, quantity, and price' + "</p>";
+        
     }
-    
-     console.log(newItem);
 
-    groceryList.push(newItem);
-    
-    
-    
-
-    //adding the array to local storage
-    window.localStorage.setItem('GroceryList', JSON.stringify(groceryList));
-    
-    
-  itemDes.value = '';
-
-    
-}
-
-else{
-    //tell themthat its not working - error messageanddo nothing
-}
-   
 }
 
 document.getElementById('addNew').addEventListener('touchend', function () {
     createList();
     totalPrice();
 })
-
-
-
-
-
-
